9ece1d3ae5d39b7487571247255aff9ca5fb0249
denyskhomiakpz2023 <denus.homiak@gmail.com>
2025-03-05 20:14:59 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".nav-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            buttons.forEach(btn => btn.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
+
